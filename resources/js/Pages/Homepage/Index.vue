24d528dfcc5ffc29178561f936f40009bdf225d4
monciego <jerichobantiquete@gmail.com>
2024-02-16 07:59:10 +0800

    feat: homepage ui

--- resources/js/Pages/Homepage/Index.vue
+++ resources/js/Pages/Homepage/Index.vue
@@ -22,27 +22,32 @@ defineProps({
 <template>
     <Head title="Welcome" />
 
-    <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-end">
-        <Link
-            v-if="$page.props.auth.user"
-            :href="route('dashboard')"
-            class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-            >Dashboard</Link
-        >
-
-        <template v-else>
-            <Link
-                :href="route('login')"
-                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                >Log in</Link
-            >
-
+    <main>
+        <h1 class="text-center font-bold text-4xl py-6 text-indigo-600">
+            STARTMO
+        </h1>
+        <div class="h-[45vh]">
+            <img src="/assets/home-image.svg" class="h-full w-full" alt="" />
+        </div>
+        <p class="font-bold tracking-wider my-4 text-center text-xl">
+            START.THINK.AIM.TELL
+        </p>
+        <div class="flex flex-col items-center justify-center gap-3">
             <Link
                 v-if="canRegister"
                 :href="route('register')"
-                class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                >Register</Link
+                class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-full px-7 py-2 text-center me-2 mb-2"
             >
-        </template>
-    </div>
+                Create Account
+            </Link>
+            <p>
+                Have an account?
+                <Link
+                    :href="route('login')"
+                    class="font-semibold underline text-indigo-600 hover:text-indigo-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500"
+                    >Log in</Link
+                >
+            </p>
+        </div>
+    </main>
 </template>
