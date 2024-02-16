9d19f0e0b6c4c1028f48d659a1d85eaed3c0b550
monciego <jerichobantiquete@gmail.com>
2024-02-16 15:08:07 +0800

    feat: add mobile navigation

---
+++ resources/js/Components/ModalForm.vue
@@ -0,0 +1,286 @@
+<script setup>
+import Modal from "@/Components/Modal.vue";
+import SuccessButton from "@/Components/SuccessButton.vue";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
+import { ref, onMounted } from "vue";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { useForm } from "@inertiajs/vue3";
+const editor = ref(ClassicEditor);
+
+const editorConfig = {
+    mediaEmbed: {
+        removeProviders: [
+            "dailymotion",
+            "spotify",
+            "youtube",
+            "vimeo",
+            "instagram",
+            "twitter",
+            "googleMaps",
+            "flickr",
+            "facebook",
+        ],
+    },
+    toolbar: [
+        "undo",
+        "redo",
+        "bold",
+        "italic",
+        "|",
+        "bulletedList",
+        "numberedList",
+        "|",
+        "heading",
+        "|",
+        "outdent",
+        "indent",
+        "|",
+        "link",
+        "|",
+        "blockQuote",
+    ],
+};
+
+onMounted(() => {
+    window.addEventListener("beforeinstallprompt", (event) => {
+        event.preventDefault();
+        deferredPrompt = event;
+        showInstallButton.value = true;
+    });
+});
+
+// Modal
+const confirmingOpeningModal = ref(false);
+
+const confirmOpenModal = () => {
+    confirmingOpeningModal.value = true;
+};
+
+const closeModal = () => {
+    confirmingOpeningModal.value = false;
+};
+
+const currentStep = ref(1);
+
+const form = useForm({
+    see: "",
+    think: "",
+    aim: "",
+    review: "",
+    tell: "",
+});
+
+const nextStep = () => {
+    if (currentStep.value < 5) {
+        currentStep.value++;
+    }
+};
+
+const prevStep = () => {
+    if (currentStep.value > 1) {
+        currentStep.value--;
+    }
+};
+
+const goToStep = (step) => {
+    currentStep.value = step;
+};
+
+const submitForm = () => {
+    // Handle form submission logic here
+    console.log("Form submitted:", formData.value);
+};
+</script>
+
+<template>
+    <button
+        @click="confirmOpenModal"
+        class="h-10 w-10 bg-indigo-700 text-white rounded-full"
+    >
+        +
+    </button>
+
+    <Modal :show="confirmingOpeningModal" @close="closeModal">
+        <form class="p-6">
+            <ol class="flex items-center w-full">
+                <li
+                    class="flex w-full items-center text-white after:content-[''] after:w-full after:h-1 after:border-b after:border-4 after:inline-block after:border-gray-700"
+                >
+                    <span
+                        @click="goToStep(1)"
+                        class="cursor-pointer flex items-center justify-center w-10 h-10 rounded-full lg:h-12 lg:w-12 shrink-0"
+                        :class="{
+                            'bg-blue-500 text-white': currentStep === 1,
+                            'bg-slate-800 text-white': currentStep !== 1,
+                        }"
+                    >
+                        S
+                    </span>
+                </li>
+                <li
+                    class="flex text-white w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-4 after:inline-block after:border-gray-700"
+                >
+                    <span
+                        @click="goToStep(2)"
+                        class="cursor-pointer flex items-center justify-center w-10 h-10 rounded-full lg:h-12 lg:w-12 shrink-0"
+                        :class="{
+                            'bg-blue-500 text-white': currentStep === 2,
+                            'bg-slate-800 text-white': currentStep !== 2,
+                        }"
+                    >
+                        T
+                    </span>
+                </li>
+                <li
+                    class="flex text-white w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-4 after:inline-block after:border-gray-700"
+                >
+                    <span
+                        @click="goToStep(3)"
+                        class="cursor-pointer flex items-center justify-center w-10 h-10 rounded-full lg:h-12 lg:w-12 shrink-0"
+                        :class="{
+                            'bg-blue-500 text-white': currentStep === 3,
+                            'bg-slate-800 text-white': currentStep !== 3,
+                        }"
+                    >
+                        A
+                    </span>
+                </li>
+                <li
+                    class="flex text-white w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-4 after:inline-block after:border-gray-700"
+                >
+                    <span
+                        @click="goToStep(4)"
+                        class="cursor-pointer flex items-center justify-center w-10 h-10 rounded-full lg:h-12 lg:w-12 shrink-0"
+                        :class="{
+                            'bg-blue-500 text-white': currentStep === 4,
+                            'bg-slate-800 text-white': currentStep !== 4,
+                        }"
+                    >
+                        R
+                    </span>
+                </li>
+                <li class="flex text-white items-center">
+                    <span
+                        @click="goToStep(5)"
+                        class="cursor-pointer flex items-center justify-center w-10 h-10 rounded-full lg:h-12 lg:w-12 shrink-0"
+                        :class="{
+                            'bg-blue-500 text-white': currentStep === 5,
+                            'bg-slate-800 text-white': currentStep !== 5,
+                        }"
+                    >
+                        T
+                    </span>
+                </li>
+            </ol>
+            <div>
+                <!-- v-show="currentStep === 1" -->
+                <div v-show="currentStep === 1">
+                    <div class="my-4">
+                        <h2>See</h2>
+                    </div>
+
+                    <ckeditor
+                        name="see"
+                        id="see"
+                        :editor="editor"
+                        v-model="form.see"
+                        :config="editorConfig"
+                    ></ckeditor>
+
+                    <div class="mt-4 flex items-center justify-end">
+                        <SuccessButton type="button" @click="nextStep">
+                            Next</SuccessButton
+                        >
+                    </div>
+                </div>
+                <div v-show="currentStep === 2">
+                    <div class="my-4">
+                        <h2>Think</h2>
+                    </div>
+
+                    <ckeditor
+                        name="think"
+                        id="think"
+                        :editor="editor"
+                        v-model="form.think"
+                        :config="editorConfig"
+                    ></ckeditor>
+
+                    <div class="flex items-center justify-between mt-4">
+                        <SecondaryButton type="button" @click="prevStep">
+                            Prev</SecondaryButton
+                        >
+                        <SuccessButton type="button" @click="nextStep">
+                            Next</SuccessButton
+                        >
+                    </div>
+                </div>
+                <div v-show="currentStep === 3">
+                    <div class="my-4">
+                        <h2>Aim</h2>
+                    </div>
+
+                    <ckeditor
+                        name="aim"
+                        id="aim"
+                        :editor="editor"
+                        v-model="form.aim"
+                        :config="editorConfig"
+                    ></ckeditor>
+
+                    <div class="flex items-center justify-between mt-4">
+                        <SecondaryButton type="button" @click="prevStep">
+                            Prev</SecondaryButton
+                        >
+                        <SuccessButton type="button" @click="nextStep">
+                            Next</SuccessButton
+                        >
+                    </div>
+                </div>
+                <div v-show="currentStep === 4">
+                    <div class="my-4">
+                        <h2>R</h2>
+                    </div>
+
+                    <ckeditor
+                        name="review"
+                        id="see"
+                        :editor="editor"
+                        v-model="form.review"
+                        :config="editorConfig"
+                    ></ckeditor>
+                    <div class="flex items-center justify-between mt-4">
+                        <SecondaryButton type="button" @click="prevStep">
+                            Prev</SecondaryButton
+                        >
+                        <SuccessButton type="button" @click="nextStep">
+                            Next</SuccessButton
+                        >
+                    </div>
+                </div>
+                <div v-show="currentStep === 5">
+                    <div class="my-4">
+                        <h2>Tell</h2>
+                    </div>
+
+                    <ckeditor
+                        name="tell"
+                        id="tell"
+                        :editor="editor"
+                        v-model="form.tell"
+                        :config="editorConfig"
+                    ></ckeditor>
+
+                    <div class="flex items-center justify-between mt-4">
+                        <SecondaryButton type="button" @click="prevStep">
+                            Prev</SecondaryButton
+                        >
+                        <SuccessButton type="submit" @click="nextStep">
+                            Submit
+                        </SuccessButton>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </Modal>
+</template>
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,11 +1,12 @@
 <script setup>
-import { ref } from 'vue';
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import Dropdown from '@/Components/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
-import NavLink from '@/Components/NavLink.vue';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import { Link } from '@inertiajs/vue3';
+import { ref } from "vue";
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import Dropdown from "@/Components/Dropdown.vue";
+import DropdownLink from "@/Components/DropdownLink.vue";
+import NavLink from "@/Components/NavLink.vue";
+import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
+import { Link } from "@inertiajs/vue3";
+import ModalForm from "@/Components/ModalForm.vue";
 
 const showingNavigationDropdown = ref(false);
 </script>
@@ -28,8 +29,13 @@ const showingNavigationDropdown = ref(false);
                             </div>
 
                             <!-- Navigation Links -->
-                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                            <div
+                                class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"
+                            >
+                                <NavLink
+                                    :href="route('dashboard')"
+                                    :active="route().current('dashboard')"
+                                >
                                     Dashboard
                                 </NavLink>
                             </div>
@@ -64,8 +70,16 @@ const showingNavigationDropdown = ref(false);
                                     </template>
 
                                     <template #content>
-                                        <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>
-                                        <DropdownLink :href="route('logout')" method="post" as="button">
+                                        <DropdownLink
+                                            :href="route('profile.edit')"
+                                        >
+                                            Profile
+                                        </DropdownLink>
+                                        <DropdownLink
+                                            :href="route('logout')"
+                                            method="post"
+                                            as="button"
+                                        >
                                             Log Out
                                         </DropdownLink>
                                     </template>
@@ -76,14 +90,23 @@ const showingNavigationDropdown = ref(false);
                         <!-- Hamburger -->
                         <div class="-me-2 flex items-center sm:hidden">
                             <button
-                                @click="showingNavigationDropdown = !showingNavigationDropdown"
+                                @click="
+                                    showingNavigationDropdown =
+                                        !showingNavigationDropdown
+                                "
                                 class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                             >
-                                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                <svg
+                                    class="h-6 w-6"
+                                    stroke="currentColor"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                >
                                     <path
                                         :class="{
                                             hidden: showingNavigationDropdown,
-                                            'inline-flex': !showingNavigationDropdown,
+                                            'inline-flex':
+                                                !showingNavigationDropdown,
                                         }"
                                         stroke-linecap="round"
                                         stroke-linejoin="round"
@@ -93,7 +116,8 @@ const showingNavigationDropdown = ref(false);
                                     <path
                                         :class="{
                                             hidden: !showingNavigationDropdown,
-                                            'inline-flex': showingNavigationDropdown,
+                                            'inline-flex':
+                                                showingNavigationDropdown,
                                         }"
                                         stroke-linecap="round"
                                         stroke-linejoin="round"
@@ -108,11 +132,17 @@ const showingNavigationDropdown = ref(false);
 
                 <!-- Responsive Navigation Menu -->
                 <div
-                    :class="{ block: showingNavigationDropdown, hidden: !showingNavigationDropdown }"
+                    :class="{
+                        block: showingNavigationDropdown,
+                        hidden: !showingNavigationDropdown,
+                    }"
                     class="sm:hidden"
                 >
                     <div class="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                        <ResponsiveNavLink
+                            :href="route('dashboard')"
+                            :active="route().current('dashboard')"
+                        >
                             Dashboard
                         </ResponsiveNavLink>
                     </div>
@@ -123,12 +153,20 @@ const showingNavigationDropdown = ref(false);
                             <div class="font-medium text-base text-gray-800">
                                 {{ $page.props.auth.user.name }}
                             </div>
-                            <div class="font-medium text-sm text-gray-500">{{ $page.props.auth.user.email }}</div>
+                            <div class="font-medium text-sm text-gray-500">
+                                {{ $page.props.auth.user.email }}
+                            </div>
                         </div>
 
                         <div class="mt-3 space-y-1">
-                            <ResponsiveNavLink :href="route('profile.edit')"> Profile </ResponsiveNavLink>
-                            <ResponsiveNavLink :href="route('logout')" method="post" as="button">
+                            <ResponsiveNavLink :href="route('profile.edit')">
+                                Profile
+                            </ResponsiveNavLink>
+                            <ResponsiveNavLink
+                                :href="route('logout')"
+                                method="post"
+                                as="button"
+                            >
                                 Log Out
                             </ResponsiveNavLink>
                         </div>
@@ -136,17 +174,137 @@ const showingNavigationDropdown = ref(false);
                 </div>
             </nav>
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow" v-if="$slots.header">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <slot name="header" />
-                </div>
-            </header>
-
             <!-- Page Content -->
             <main>
                 <slot />
             </main>
+
+            <div
+                class="fixed z-50 w-full h-16 bottom-0 left-0 right-0 bg-gray-950"
+            >
+                <div class="grid h-full max-w-lg grid-cols-5 mx-auto">
+                    <button
+                        data-tooltip-target="tooltip-home"
+                        type="button"
+                        class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+                    >
+                        <svg
+                            class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                        >
+                            <path
+                                d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"
+                            />
+                        </svg>
+                        <span class="sr-only">Home</span>
+                    </button>
+                    <div
+                        id="tooltip-home"
+                        role="tooltip"
+                        class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+                    >
+                        Home
+                        <div class="tooltip-arrow" data-popper-arrow></div>
+                    </div>
+                    <button
+                        data-tooltip-target="tooltip-wallet"
+                        type="button"
+                        class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+                    >
+                        <svg
+                            class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                        >
+                            <path
+                                d="M11.074 4 8.442.408A.95.95 0 0 0 7.014.254L2.926 4h8.148ZM9 13v-1a4 4 0 0 1 4-4h6V6a1 1 0 0 0-1-1H1a1 1 0 0 0-1 1v13a1 1 0 0 0 1 1h17a1 1 0 0 0 1-1v-2h-6a4 4 0 0 1-4-4Z"
+                            />
+                            <path
+                                d="M19 10h-6a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1Zm-4.5 3.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM12.62 4h2.78L12.539.41a1.086 1.086 0 1 0-1.7 1.352L12.62 4Z"
+                            />
+                        </svg>
+                        <span class="sr-only">Wallet</span>
+                    </button>
+                    <div
+                        id="tooltip-wallet"
+                        role="tooltip"
+                        class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+                    >
+                        Wallet
+                        <div class="tooltip-arrow" data-popper-arrow></div>
+                    </div>
+                    <div class="flex relative items-center justify-center">
+                        <button
+                            data-tooltip-target="tooltip-new"
+                            type="button"
+                            class="inline-flex items-center justify-center w-16 h-16 font-medium rounded-full"
+                        >
+                            <ModalForm />
+                        </button>
+                    </div>
+                    <button
+                        data-tooltip-target="tooltip-settings"
+                        type="button"
+                        class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+                    >
+                        <svg
+                            class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 20 20"
+                        >
+                            <path
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2"
+                            />
+                        </svg>
+                        <span class="sr-only">Settings</span>
+                    </button>
+                    <div
+                        id="tooltip-settings"
+                        role="tooltip"
+                        class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+                    >
+                        Settings
+                        <div class="tooltip-arrow" data-popper-arrow></div>
+                    </div>
+                    <button
+                        data-tooltip-target="tooltip-profile"
+                        type="button"
+                        class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+                    >
+                        <svg
+                            class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                        >
+                            <path
+                                d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"
+                            />
+                        </svg>
+                        <span class="sr-only">Profile</span>
+                    </button>
+                    <div
+                        id="tooltip-profile"
+                        role="tooltip"
+                        class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+                    >
+                        Profile
+                        <div class="tooltip-arrow" data-popper-arrow></div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
