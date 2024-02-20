0f7323895c0bc6063b7485648532e0dc6f4ac1cc
monciego <jerichobantiquete@gmail.com>
2024-02-21 02:04:09 +0800

    feat: display details

--- app/Http/Controllers/DirectoryController.php
+++ app/Http/Controllers/DirectoryController.php
@@ -11,10 +11,8 @@ class DirectoryController extends Controller
 {
     public function index() {
         $userId = auth()->user()->id;
-        // this is for admin
         $documents = Start::with('user')->latest()->get();
-        // this is for user
-        $yourDocuments = Start::where('user_id', $userId )->get();
+        $yourDocuments = Start::where('user_id', $userId )->latest()->get();
         return Inertia::render("User/Directory", compact('documents', 'yourDocuments'));
     }
 
--- resources/js/Pages/User/Directory/Show.vue
+++ resources/js/Pages/User/Directory/Show.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { Head, Link } from "@inertiajs/vue3";
+import { Head } from "@inertiajs/vue3";
 
 defineProps(["directory"]);
 
@@ -12,5 +12,68 @@ dayjs.extend(LocalizedFormat);
 
 <template>
     <Head title="Directory" />
-    <AuthenticatedLayout> {{ directory }}</AuthenticatedLayout>
+    <AuthenticatedLayout>
+        <ol class="relative mt-10 mx-6 sm:mx-10 border-s border-gray-700">
+            <li class="mb-10 ms-10">
+                <span
+                    class="absolute flex items-center justify-center w-6 h-6 rounded-full -start-3 ring-8 ring-yellow-500 bg-indigo-600 text-white"
+                >
+                    S
+                </span>
+                <h3 class="mb-2 text-lg font-semibold text-gray-900">SEE</h3>
+                <div
+                    class="prose text-base font-normal text-gray-700"
+                    v-html="directory.see"
+                ></div>
+            </li>
+            <li class="mb-10 ms-10">
+                <span
+                    class="absolute flex items-center justify-center w-6 h-6 rounded-full -start-3 ring-8 ring-yellow-500 bg-indigo-600 text-white"
+                >
+                    T
+                </span>
+                <h3 class="mb-2 text-lg font-semibold text-gray-900">THINK</h3>
+                <div
+                    class="prose text-base font-normal text-gray-700"
+                    v-html="directory.think"
+                ></div>
+            </li>
+            <li class="mb-10 ms-10">
+                <span
+                    class="absolute flex items-center justify-center w-6 h-6 rounded-full -start-3 ring-8 ring-yellow-500 bg-indigo-600 text-white"
+                >
+                    A
+                </span>
+                <h3 class="mb-2 text-lg font-semibold text-gray-900">AIM</h3>
+                <div
+                    class="prose text-base font-normal text-gray-700"
+                    v-html="directory.aim"
+                ></div>
+            </li>
+            <li class="mb-10 ms-10">
+                <span
+                    class="absolute flex items-center justify-center w-6 h-6 rounded-full -start-3 ring-8 ring-yellow-500 bg-indigo-600 text-white"
+                >
+                    R
+                </span>
+                <h3 class="mb-2 text-lg font-semibold text-gray-900">REFINE</h3>
+                <div
+                    class="prose text-base font-normal text-gray-700"
+                    v-html="directory.refine"
+                ></div>
+            </li>
+            <li class="mb-10 ms-10">
+                <span
+                    class="absolute flex items-center justify-center w-6 h-6 rounded-full -start-3 ring-8 ring-yellow-500 bg-indigo-600 text-white"
+                >
+                    T
+                </span>
+                <h3 class="mb-2 text-lg font-semibold text-gray-900">TELL</h3>
+                <div
+                    class="prose text-base font-normal text-gray-700"
+                    v-html="directory.tell"
+                ></div>
+            </li>
+        </ol>
+    </AuthenticatedLayout>
 </template>
