32978be5027e36c96d1a11eff5e6a089fd10c349
monciego <jerichobantiquete@gmail.com>
2024-03-05 10:28:12 +0800

    build files

---
+++ public/build/assets/ApplicationLogo--m4su1-p.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper-x3n3nnut.js";import{o as t,c as e}from"./app-MFLQ4sW7.js";const n={},a={class:"h-20",src:"https://upload.wikimedia.org/wikipedia/en/7/75/Pangasinan_State_University_logo.png",alt:""};function c(i,s){return t(),e("img",a)}const p=o(n,[["render",c]]);export{p as A};
