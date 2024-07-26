c536c60f11ea806236268da06019727d5bd40196
monciego <jerichobantiquete@gmail.com>
2024-07-26 12:57:51 +0800

    feat: add description

--- resources/js/Components/Introduction.vue
+++ resources/js/Components/Introduction.vue
@@ -3,16 +3,22 @@
 <template>
     <div class="pb-10 sm:pb-16">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
-            <div class="mx-auto max-w-2xl lg:text-center">
+            <div class="mx-auto max-w-5xl lg:text-center">
                 <p
                     class="mt-2 text-2xl sm:text-3xl font-bold tracking-tight text-gray-90"
                 >
-                    Everything you need to know about iSTART
+                    Everything you need to know about iSTART approach
                 </p>
-                <p class="mt-6 text-base sm:text-lg sm:leading-8 text-gray-600">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Eaque fuga corrupti rerum repudiandae ad? Non totam facere
-                    deserunt suscipit esse?
+                <p class="mt-6 text-base sm:leading-8 text-gray-600">
+                    The iSTARTMO Approach is version 2.0 of START Approach first
+                    published in 2019, then refined in 2023, by Dr. Jun S.
+                    Camara, a faculty of Pangasinan State University,
+                    Philippines. The main purpose of the START Approach is to
+                    develop a research topic from which a research title could
+                    be generated. Because of the progress of the approach when
+                    it is pilot-tested in selected high schools, the iSTARTMO
+                    was subsequently developed to include mentoring and
+                    outlining.
                 </p>
             </div>
             <div class="mt-16 mx-auto max-w-2xl lg:max-w-4xl">
@@ -31,9 +37,10 @@
                             SEE
                         </dt>
                         <dd class="mt-2 text-base leading-7 text-gray-600">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Vitae debitis quos sequi tenetur labore
-                            voluptas.
+                            Using human senses and tools to ‘see’ an object in
+                            the immediate environment. (Note: The use of ‘tools’
+                            as an aid to the human eye is used when the class is
+                            an advance class in ‘Under Cell Biology’
                         </dd>
                     </div>
                     <div class="relative pl-16">
@@ -48,9 +55,11 @@
                             THINK
                         </dt>
                         <dd class="mt-2 text-base leading-7 text-gray-600">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Qui repellat accusamus velit tempora ipsam
-                            enim.
+                            Mentally outlining around 3-4 ‘under the sun’ (UTS)
+                            things that come to mind while looking at the SEEn
+                            object (i.e. clearer focus on size, shape, color,
+                            location, imprints, personal experience, and any of
+                            the object’s attributes)
                         </dd>
                     </div>
                     <div class="relative pl-16">
@@ -65,9 +74,10 @@
                             AIM
                         </dt>
                         <dd class="mt-2 text-base leading-7 text-gray-600">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Et recusandae accusantium tenetur, repellat
-                            doloremque velit?
+                            Choosing only one among the OUTLINEd UTS to
+                            specifically focus at, while putting others aside
+                            (i.e. intent to have closer focus based on whatever
+                            personal reasons using ‘Wh’ questions)
                         </dd>
                     </div>
                     <div class="relative pl-16">
@@ -82,9 +92,11 @@
                             REFINE
                         </dt>
                         <dd class="mt-2 text-base leading-7 text-gray-600">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Asperiores dignissimos impedit quasi mollitia
-                            numquam odio.
+                            Using this AIMed focus, identify a verb that could
+                            specify the doable aspects. (Note: Students know
+                            verbs because they already met this in their English
+                            subjects. This is a crucial step, help your
+                            students. Many fail in this step.)
                         </dd>
                     </div>
                     <div class="relative pl-16">
@@ -99,9 +111,8 @@
                             TELL
                         </dt>
                         <dd class="mt-2 text-base leading-7 text-gray-600">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Quisquam vero numquam similique, at neque
-                            quibusdam!
+                            Using the VERBed focus, share what you want to ‘do’
+                            (i.e. methodology) to others
                         </dd>
                     </div>
                     <div class="relative pl-16">
@@ -116,9 +127,9 @@
                             Mentoring & Outlining
                         </dt>
                         <dd class="mt-2 text-base leading-7 text-gray-600">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Ipsum iure blanditiis assumenda nisi, nulla
-                            perspiciatis.
+                            Wait for feedback from your mentor and once feedback
+                            is received, you will outline your steps to start
+                            and end the study.
                         </dd>
                     </div>
                 </dl>
